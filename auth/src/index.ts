7ca3446ec305e9d2fd4862c9ca558dd9aded1134
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-08 19:59:27 +0000

    create routing structure

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
 
 const PORT = 4000;
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hello');
-});
+app.use(currentUserRouter);
 
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`));
---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('api/users/currentuser', (req, res) => {});
+
+export { router as currentUserRouter };
