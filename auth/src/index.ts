5328debe84db3a349656a891eca4e0fdb9d90c9e
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-08 20:07:20 +0000

    add auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const PORT = 4000;
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(PORT, () => console.log(`App running on port: ${PORT}`));
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('api/users/signup', (req, res) => {});
+
+export { router as signupRouter };
