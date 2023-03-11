8968e3d09632249ffe4d76891804d7a84c1ca03f
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-11 22:57:08 +0000

    signin logic

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {});
+router.post('/api/users/signout', (req, res) => {
+  req.session = null;
+
+  res.status(200).send({});
+});
 
 export { router as signoutRouter };
