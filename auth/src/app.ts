4cf8e4ad45981d8880152b4a80b6b3bf24be61b5
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-13 19:01:03 +0000

    auth tests

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -19,7 +19,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: true,
+    secure: process.env.NODE_ENV !== 'test',
     maxAge: 1 * 60 * 60 * 1000, // 1 hour
   })
 );
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,6 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
 
 let mongo: MongoMemoryServer;
@@ -24,3 +25,24 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
