4a226eb69ef788493f1658246903fd7bb05cf87a
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-14 22:04:52 +0000

    header logic

---
+++ client/components/header.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && {
+      label: 'Sign Up',
+      href: 'auth/signup',
+    },
+    !currentUser && {
+      label: 'Sign In',
+      href: 'auth/signin',
+    },
+    currentUser && {
+      label: 'Sign Out',
+      href: 'auth/signout',
+    },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className="nav-item">
+          <Link href={href}>{label}</Link>
+        </li>
+      );
+    });
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <Link href="/" className="navbar-brand">
+        Gettix
+      </Link>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
