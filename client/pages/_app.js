bb1bd4a84931f737b1257bec77bccc61bfc79f34
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-14 21:42:47 +0000

    sorting initial props for app and components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Hello {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+// initial props for app
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+
+  // initial props for individual pages
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { ...data, pageProps };
+};
+
+export default AppComponent;
