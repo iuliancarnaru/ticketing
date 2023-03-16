beecb01a41444e5173bd00132a4b1af8f0326ae9
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-16 16:30:59 +0000

    set mongo uri as env variable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,8 +8,12 @@ async function main() {
     throw new Error('JWT_KEY must be defined');
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to Auth-MongoDb');
   } catch (error) {
     console.error(error);
