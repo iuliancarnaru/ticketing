ee1b06e183d3f03416497878e689f465d2ead12e
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-21 17:36:11 +0000

    update route for ticket update

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,11 +7,13 @@ import {
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.put(
-  '/api/tickets',
+  '/api/tickets/:id',
   requireAuth,
   [
     body('title').not().isEmpty().withMessage('Title is required'),
@@ -37,6 +39,12 @@ router.put(
     });
 
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
