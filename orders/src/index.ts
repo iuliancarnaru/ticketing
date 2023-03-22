37b095e7b10bfedcd059df88684f504c9fbee7e7
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-03-22 19:20:03 +0000

    add orders service

---
+++ orders/src/index.ts
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const PORT = 4002;
+
+async function main() {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  try {
+    // connect to NATS
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+
+    // watching for interrupt or terminated signals
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    // connect to MONGOOSE
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to Tickets-MongoDb');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(PORT, () =>
+    console.log(`Tickets service running on port: ${PORT}`)
+  );
+}
+
+main();
