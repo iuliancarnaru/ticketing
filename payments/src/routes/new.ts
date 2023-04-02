5d6f491c17b5d35c76e2d0b0178cbc47f88203a0
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-04-02 21:11:59 +0100

    integrate stripe

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus,
 } from '@tkts/common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -33,6 +34,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.send({ success: true });
   }
