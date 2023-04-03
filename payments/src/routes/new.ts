11d9182a2d6e3c5b81701bdb69d4be9dd8700155
Iulian Carnaru <iulian.carnaru@gmail.com>
2023-04-03 20:35:50 +0100

    add publisher and listener for payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import {
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
 import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -49,7 +51,13 @@ router.post(
 
     await payment.save();
 
-    res.status(201).send({ success: true });
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.id,
+    });
+
+    res.status(201).send({ id: payment.id });
   }
 );
 
